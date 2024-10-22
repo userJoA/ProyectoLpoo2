62932fcb1dcff97b64aedf131dd96742b2967ec4
userJoA <jo1gallardo@gmail.com>
2024-10-22 07:43:59 -0300

    tp3_p2

---
+++ ClasesBase/TrabajarCompetencias.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClasesBase
+{
+    public class TrabajarCompetencias
+    {
+        public DataTable getCompetencies()
+        {
+            using (DataTable dt = new DataTable())
+            {
+                using (SqlConnection cn = new SqlConnection(ClasesBase.Properties.Settings.Default.DBApp))
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter("Select * from Competencia", cn))
+                    {
+                        da.Fill(dt);
+                    }
+                }
+                return dt;
+            }
+
+        }
+
+    }
+}
