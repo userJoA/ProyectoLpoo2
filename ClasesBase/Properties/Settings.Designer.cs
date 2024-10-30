941907d97e78d022adc21647494c0efd791f43fc
Gallardo Alejandro Joaquin <97065359+userJoA@users.noreply.github.com>
2024-10-30 13:10:06 -0300

    Correcion BD

--- ClasesBase/Properties/Settings.Designer.cs
+++ ClasesBase/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ClasesBase.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace ClasesBase.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\usuario\\source\\repos" +
-            "\\ProyectoLpoo2\\ComDep.mdf;Integrated Security=True;Connect Timeout=30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\jo1ga\\source\\repos\\P" +
+            "royectoLpoo2\\ComDep.mdf;Integrated Security=True;Connect Timeout=30")]
         public string DBApp {
             get {
                 return ((string)(this["DBApp"]));
