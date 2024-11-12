46e6bfdcd6088afc426292a54d81adc2a1a81046
Gallardo Alejandro Joaquin <97065359+userJoA@users.noreply.github.com>
2024-11-12 17:54:59 -0300

    tp4_b

--- Vistas/UserControls/vistasUsuario/ListaDeUsuario.xaml.cs
+++ Vistas/UserControls/vistasUsuario/ListaDeUsuario.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClasesBase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,34 @@ namespace Vistas.UserControls.vistasUsuario
     /// </summary>
     public partial class ListaDeUsuario : UserControl
     {
+        private CollectionViewSource vistaColeccionFiltrada;
         public ListaDeUsuario()
         {
             InitializeComponent();
+
+            vistaColeccionFiltrada = Resources["VISTA_USER"] as CollectionViewSource;
+        }
+
+        private void txtUsuarioBusqueda_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (vistaColeccionFiltrada != null)
+            {
+                vistaColeccionFiltrada.Filter += eventVistaUsuario_Filter;
+            }
+        }
+
+        private void eventVistaUsuario_Filter(object sender, FilterEventArgs e) 
+        {
+            Usuario usuario = e.Item as Usuario;
+            if(usuario.Usu_Name.StartsWith(txtUsuarioBusqueda.Text,StringComparison.CurrentCultureIgnoreCase))
+            {
+                e.Accepted = true;
+            }
+            else
+            {
+                e.Accepted &= false;
+            }
+
         }
     }
 }
